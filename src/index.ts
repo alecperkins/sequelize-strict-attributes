28939a35619874d168a6ee089b25125b8d24c619
Alec Perkins <mail@alecperkins.me>
2023-03-23 19:28:14 -0400

    feat: Guard against .get access

--- src/index.ts
+++ src/index.ts
@@ -36,4 +36,15 @@ function guardAttributes (instance) {
     }
   });
 
+  // Intercept indirect .get access of the attributes.
+  const actualGet = instance.get.bind(instance);
+  function get (key, options) {
+    if (!loaded_attrs.has(key)) {
+      throw new InstanceError(`Cannot access attribute ${ key } on ${ instance.constructor.name } omitted from attributes`);
+    }
+    return actualGet(key, options);
+  }
+  instance.get = get.bind(instance);
+
+
 }
