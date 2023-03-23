3267537adec153bb138fde9d18e70adef5d9dc5a
Alec Perkins <mail@alecperkins.me>
2023-03-23 19:22:13 -0400

    feat: Guard against omitted attributes in findOne

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-export {}
+import { InstanceError, Model } from "sequelize";
+import type { Sequelize } from "sequelize";
+
+/**
+ * @param sequelize - an active Sequelize instance
+ */
+export default function sequelizeStrictAttributes (sequelize: Sequelize) {
+  sequelize.addHook('afterFind', (result) => {
+    if (result) {  // Make sure it's not an empty result from a findOne
+      guardAttributes(result);
+    }
+  });
+}
+
+function guardAttributes (instance) {
+  const loaded_attrs = new Set([
+    ...instance._options.attributes,
+  ]);
+
+  // Block direct dot-notation access of the attributes.
+  Object.keys(instance.constructor.rawAttributes).forEach(key => {
+    if (!loaded_attrs.has(key)) {
+      Object.defineProperty(instance, key, {
+        ...instance.constructor._attributeManipulation[key], // Keep set() and the rest in place
+        get () {
+          throw new InstanceError(`Cannot access attribute ${ key } on ${ instance.constructor.name } omitted from attributes`);
+        },
+      });
+    }
+  });
+
+}
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,58 @@
 
 import { describe, expect, test } from "vitest";
+import { Sequelize, DataTypes, Model, BelongsToGetAssociationMixin} from 'sequelize';
 
-describe("", () => {
-  test("", async () => {
+import sequelizeStrictAttributes from '../src';
 
+describe("sequelizeStrictAttributes", () => {
+
+  test("guards against use of omitted attributes after findOne", async () => {
+    const { sequelize, Foo } = await sharedSetup();
+    sequelizeStrictAttributes(sequelize);
+    expect((await Foo.count())).toBe(1);
+    
+    const result = await Foo.findOne({
+      where: { alpha: "abc" },
+      attributes: ["alpha"],
+      rejectOnEmpty: true,
+    });
+    
+    expect(() => result.bravo).toThrow("Cannot access attribute bravo on Foo omitted from attributes");
+    expect(() => result.get("bravo")).toThrow("Cannot access attribute bravo on Foo omitted from attributes");
+    expect(() => result.bravo += 1).toThrow("Cannot access attribute bravo on Foo omitted from attributes");
+
+    expect(result.alpha).toEqual("abc");
+    expect(result.get("alpha")).toEqual("abc");
+    expect(() => result.bravo = 1).not.toThrow();
+    expect(() => result.set("bravo", 1)).not.toThrow();
+  });
   });
 });
+
+
+async function sharedSetup () {
+  const sequelize = new Sequelize('sqlite::memory:');
+  await sequelize.authenticate();
+  class Foo extends Model {
+  }
+    alpha: string;
+    bravo: number;
+  Foo.init({
+    alpha: DataTypes.STRING,
+    bravo: DataTypes.INTEGER,
+  }, {
+    sequelize,
+  });
+  await sequelize.sync({ force: true });
+
+  const foo = await Foo.create({
+    alpha: "abc",
+    bravo: 4,
+  });
+
+  return {
+    sequelize,
+    Foo,
+  };
+}
+
